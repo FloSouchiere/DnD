37dab06dce715225e65b45112660011ded7d54e2
FloSouchiere <59911947+FloSouchiere@users.noreply.github.com>
2020-05-05 11:31:12 +0200

    Update Wizard.java

--- src/Personnages/Wizard.java
+++ src/Personnages/Wizard.java
@@ -1,5 +1,8 @@
 package Personnages;
 
+/** As its equivalent Warrior Class, Wizard class is sub to Character to allow Characters to be stored in a list.
+ * It polymorphes Character, adding its own witchcraft and accessories.*/
+
 public class Wizard extends Character {
 
 	// Attributes
@@ -19,6 +22,9 @@ public class Wizard extends Character {
 		String str = super.getSimpleName()+" the Jizzhard";
 		return str;
 	}
+	
+/** Intended to ease the console display of the characters' list elements and appear
+* as nice and complete as possible.*/
 
 	public String toString() {
 		return ("I'm " + this.getName()+"\ni can cast " + spell.getName() + " and drink a " + this.philter
