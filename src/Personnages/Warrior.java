f849aec9a6ca5ee4365d9d054c48d9d0225d4ab3
FloSouchiere <59911947+FloSouchiere@users.noreply.github.com>
2020-05-05 11:29:56 +0200

    Update Warrior.java

--- src/Personnages/Warrior.java
+++ src/Personnages/Warrior.java
@@ -1,11 +1,16 @@
 package Personnages;
 
+/** As its equivalent Wizard Class, Warrior class is sub to Character to allow Characters to be stored in a list.
+ * It polymorphes Character, adding its own weaponry and accessories.*/
+
 public class Warrior extends Character {
 
 	// Attributes
 
 	private Weapon weapon;
 	private String shield;
+	
+	// Constructor
 
 	public Warrior(String name, String url, int life, int strength, Weapon weapon, String shield) {
 		super(name, url, life, strength);
@@ -17,6 +22,9 @@ public class Warrior extends Character {
 		String str = super.getSimpleName() + " the Social Justice Warrior";
 		return str;
 	}
+	
+/** Intended to ease the console display of the characters' list elements and appear
+ * as nice and complete as possible.*/
 
 	public String toString() {
 		return ("I'm " + this.getName() + "\ni can use " + this.weapon.getName() + " and a " + this.shield
